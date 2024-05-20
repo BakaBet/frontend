4b486bbd039f7d2beabc103b81d37cf256100906
Guillaume KLEIN <kleinguillaume005@gmail.com>
2024-05-20 21:02:32 +0200

    header, footer, accueil

--- Bakafront/src/app/app.routes.ts
+++ Bakafront/src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [  
+  { path: 'accueil', component: HomeComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
