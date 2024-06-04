d6c1438dcb45a9bb14cf330875b51ddd766216a1
Guillaume KLEIN <kleinguillaume005@gmail.com>
2024-06-04 08:54:55 +0200

    Liam n'a plus besoin de l'API key

--- Bakafront/src/app/environments/environments.ts
+++ Bakafront/src/app/environments/environments.ts
@@ -1,7 +1,4 @@
 export const environment = {
     production: false,
     backendClient : './assets/matchs.json',
-    SportsOddsApi: {
-      ApiKey: "62a9e964acb1b087bfc057fe3f07947d"
-    }
   };
